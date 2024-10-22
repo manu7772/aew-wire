69bc2ddc947bd7d7330e8c1d1d334bd3c7956a53
manu7772 <manu7772@gmail.com>
2024-10-22 19:22:01 +0000

    Too mush files tested for asset mapper Cleanup

--- assets/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-import wireadmin_controller from './dist/wireadmin_controller.js'
-
-const wireadmin = startStimulusApp();
-// register any custom, 3rd party controllers here
-wireadmin.register('wireadmin', wireadmin_controller);
