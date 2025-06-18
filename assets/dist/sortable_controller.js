e7f6619ae063bc138e1a584912a159812922e0c8
edujardin <manu7772@gmail.com>
2025-06-18 21:12:28 +0200

    pre-install sortablejs

---
+++ assets/dist/sortable_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus'
+import Sortable from 'sortablejs'
+
+/**
+ * @see https://github.com/SortableJS/Sortable/tree/1.15.6 -- GitHub --
+ * @see https://www.npmjs.com/package/sortablejs -- npm --
+ * @see https://sortablejs.github.io/Sortable/ -- demo --
+ * @see https://youtu.be/FKAMRLQpypk?si=GEM8Y3ueZIkz2Jjg -- video --
+ */
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+
+    connect() {
+        
+    }
+
+    disconnect() {
+
+    }
+
+}
